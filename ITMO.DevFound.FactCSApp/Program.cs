df70ebf35f2e301a3ec964b1598824685c59791e
SVTsvetkov <tsvetkov71@gmail.com>
2022-04-14 14:56:21 +0300

    Add project files.

---
+++ ITMO.DevFound.FactCSApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.DevFound.FactCSApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int F = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                F *= i;
+            }
+            Console.WriteLine(F);
+        }
+    }
+}
